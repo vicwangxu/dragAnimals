afa7c7bbd6903ca9fe7c195cad28f16f17538076
xu wang <xuwang@vpl574.wireless-pennnet.upenn.edu>
2012-08-30 18:40:13 -0400

    Initial Commit

---
+++ SpriteRun/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SpriteRun
+//
+//  Created by xu wang on 8/30/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
