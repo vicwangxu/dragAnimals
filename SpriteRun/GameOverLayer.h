1884e1e48aaa5a4141836365fa4a3cc474d6172b
vicwangxu <victorwang0924@gmail.com>
2012-09-05 20:22:16 -0400

    Changes for animals

---
+++ SpriteRun/GameOverLayer.h
@@ -0,0 +1,13 @@
+//
+//  GameOverLayer.h
+//  SpriteRun
+//
+//  Created by xu wang on 8/31/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameOverLayer : NSObject
+
+@end
---
+++ SpriteRun/GameOverLayer.m
@@ -0,0 +1,13 @@
+//
+//  GameOverLayer.m
+//  SpriteRun
+//
+//  Created by xu wang on 8/31/12.
+//
+//
+
+#import "GameOverLayer.h"
+
+@implementation GameOverLayer
+
+@end
